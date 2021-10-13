ce318b9395c10b5de29aa78c2111b7635a04a541
r-kyle-kennedy <r.kyle.kennedy@gmail.com>
2021-10-13 16:07:58 -0700

    view any book by id added

--- app.py
+++ app.py
@@ -59,6 +59,28 @@ def clean_price(price_str):
         return int(price_float * 100)
 
 
+def clean_id(id_str, id_options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+        \n***** ID Error *****
+        \rThe id should be a number
+        \rPress enter to try again.
+        \r**********************''')
+        return
+    else:
+        if book_id in id_options:
+            return book_id
+        else:
+            input(f'''
+            \n***** ID Error *****
+            \rOptions: {id_options}
+            \rPress enter to try again.
+            \r**********************''')
+            return
+
+
 def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -102,10 +124,25 @@ def app():
         elif choice == '2':
             #view books
             for book in session.query(Book):
-                print(f'{book.id} | {book.title} | {book.author} | {book.published_date} | {book.price}')
+                print(f'{book.id} | {book.title} | {book.author} | {book.published_date} | ${book.price/100}')
             input('\nPress enter to return to the main menu.')
         elif choice == '3':
-            pass
+            #search book
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f'''
+                    \nId options: {id_options}
+                    \rBook id: ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            found_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'\n{found_book.id} | {found_book.title} | {found_book.author} | {found_book.published_date} | ${found_book.price/100}')
+            input('Press enter to return to main menu.')
+
         elif choice == '4':
             pass
         else:
